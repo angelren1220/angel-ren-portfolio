bba2fef70131b7d63a19e622e96819d857adf9b3
Linxue <angelren1220@gmail.com>
2023-09-09 03:10:31 -0400

    break down previous index and styles

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+import { WelcomeSectionComponent } from './welcome-section/welcome-section.component';
+import { ProjectsSectionComponent } from './projects-section/projects-section.component';
+import { ContactSectionComponent } from './contact-section/contact-section.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    WelcomeSectionComponent,
+    ProjectsSectionComponent,
+    ContactSectionComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/welcome-section/welcome-section.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-welcome-section',
+  templateUrl: './welcome-section.component.html',
+  styleUrls: ['./welcome-section.component.less']
+})
+export class WelcomeSectionComponent {
+
+}
