d8897363e996939278e84f544357403a6ad07482
Linxue <angelren1220@gmail.com>
2023-09-13 01:38:40 -0400

    refactor contacts section

--- src/app/contact-section/contact-section.component.ts
+++ src/app/contact-section/contact-section.component.ts
@@ -6,5 +6,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./contact-section.component.less']
 })
 export class ContactSectionComponent {
+    contactLinks = [
+      { 
+        type: 'LinkedIn',
+        link: 'https://www.linkedin.com/in/linxue-angel-ren/',
+        icon: 'fab fa-linkedin' 
+      },
 
+      {
+        type: 'Github',
+        link: 'https://github.com/angelren1220',
+        icon: 'fab fa-github'
+      },
+
+      {
+        type: 'Email',
+        link: 'mailto:angelren1220@gmail.com',
+        icon: 'fas fa-at'
+      },
+
+      {
+        type: 'Phone',
+        link: 'tel:519-701-3744',
+        icon: 'fas fa-mobile-alt'
+      }
+    ];
 }
