d5596b4278ca482f9e6fd4ae07b14e581fa09264
Linxue <angelren1220@gmail.com>
2023-09-13 02:31:52 -0400

    refactor welcome-section

--- src/app/welcome-section/welcome-section.component.ts
+++ src/app/welcome-section/welcome-section.component.ts
@@ -6,5 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./welcome-section.component.less']
 })
 export class WelcomeSectionComponent {
-
+  public skillSets = [
+    "JavaScript | TypeScript | ReactJS | Vue | Angular | jQuery | D3",
+    "NodeJS | Express | Ruby | Rails | Java | Python | C#",
+    "HTML | CSS | SASS | Bootstrap",
+    "SQL | PostgreSQL",
+    "Cypress | RSpec | Jest | Mocha | Chai | Storybook",
+  ];
+  
 }
