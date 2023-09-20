ddc5fc31cc1d4808e6c60a80cabe94356bc870a5
Linxue <angelren1220@gmail.com>
2023-09-20 03:49:36 -0400

    add photo

--- src/app/welcome-section/welcome-section.component.ts
+++ src/app/welcome-section/welcome-section.component.ts
@@ -6,6 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./welcome-section.component.less']
 })
 export class WelcomeSectionComponent {
+  public imageLink = "https://media.licdn.com/dms/image/D5603AQH27i1Ga90Otw/profile-displayphoto-shrink_800_800/0/1695178603326?e=2147483647&v=beta&t=9Smx3_cfDUFNZZq_gKeSkmBFFJm2ynC8JpNRtdbR6Bg";
+
   public skillSets = [
     "JavaScript | TypeScript | ReactJS | Vue | Angular | jQuery | D3",
     "NodeJS | Express | Ruby | Rails | Java | Python | C#",
