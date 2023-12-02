cbfc2dc60e68dd85699cf02014862e82287b6e3d
Linxue <angelren1220@gmail.com>
2023-12-01 20:31:57 -0500

    changed image link

--- src/app/welcome-section/welcome-section.component.ts
+++ src/app/welcome-section/welcome-section.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./welcome-section.component.less']
 })
 export class WelcomeSectionComponent {
-  public imageLink = "https://media.licdn.com/dms/image/D5603AQH27i1Ga90Otw/profile-displayphoto-shrink_800_800/0/1695178603326?e=2147483647&v=beta&t=9Smx3_cfDUFNZZq_gKeSkmBFFJm2ynC8JpNRtdbR6Bg";
+  public imageLink = "https://media.licdn.com/dms/image/D5603AQFMPrhuI3r5qg/profile-displayphoto-shrink_400_400/0/1701471972607?e=1706745600&v=beta&t=47NMvges6KEdNkPwcH4dcwzoufySeLbidB6havMqVRQ";
 
   public skillSets = [
     "JavaScript | TypeScript | ReactJS | Vue | Angular | jQuery | D3",
